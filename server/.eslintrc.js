a0395ace1cd0e34ef1599e77f4577a18da13b73b
th92rodr <45070690+th92rodr@users.noreply.github.com>
2020-06-02 17:28:17 -0300

    Server: add eslint config file

---
+++ server/.eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es6: true,
+    node: true,
+  },
+  extends: 'eslint:recommended',
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+};
