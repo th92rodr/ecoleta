376440460a8106173defc5a9da26212ce645a306
Thiago92Rodrigues <thiago.clrodrigues@gmail.com>
2020-06-02 17:29:30 -0300

    run prettier

--- server/src/controllers/items.ts
+++ server/src/controllers/items.ts
@@ -1,11 +1,11 @@
-import { Request, Response } from "express";
+import { Request, Response } from 'express';
 
-import knex from "../database/connection";
+import knex from '../database/connection';
 
 class ItemsController {
   async index(req: Request, res: Response) {
-    const items = await knex("items").select("*");
-    const serializedItems = items.map((i) => {
+    const items = await knex('items').select('*');
+    const serializedItems = items.map(i => {
       return {
         id: i.id,
         title: i.title,
--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,17 +1,17 @@
-import express from "express";
+import express from 'express';
 
-import PointsController from "../controllers/points";
-import ItemsController from "../controllers/items";
+import PointsController from '../controllers/points';
+import ItemsController from '../controllers/items';
 
 const router = express.Router();
 
 const pointsController = new PointsController();
 const itemsController = new ItemsController();
 
-router.get("/items", itemsController.index);
+router.get('/items', itemsController.index);
 
-router.post("/points", pointsController.create);
-router.get("/points", pointsController.index);
-router.get("/points/:id", pointsController.show);
+router.post('/points', pointsController.create);
+router.get('/points', pointsController.index);
+router.get('/points/:id', pointsController.show);
 
 export default router;
