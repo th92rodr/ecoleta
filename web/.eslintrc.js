a7dbf885d26ca17e0fb7e8fcd39a615d6a1fb2a7
Thiago92Rodrigues <thiago.clrodrigues@gmail.com>
2020-06-05 17:19:43 -0300

    add eslint config file

---
+++ web/.eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: 'eslint:recommended',
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+      },
+      ecmaVersion: 2018,
+      sourceType: 'module',
+    },
+  },
+};
