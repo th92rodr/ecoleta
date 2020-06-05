9f4f2c47eb2b29b4e3712b918720776821624c21
th92rodr <45070690+th92rodr@users.noreply.github.com>
2020-06-05 17:06:29 -0300

    Server: add missed return statement in db migrations

--- server/src/database/migrations/02_create_point_items.ts
+++ server/src/database/migrations/02_create_point_items.ts
@@ -1,7 +1,7 @@
 import Knex from 'knex';
 
 export async function up(knex: Knex) {
-  knex.schema.createTable('point_items', table => {
+  return knex.schema.createTable('point_items', table => {
     table.increments('id').primary();
     table.integer('point_id').notNullable().references('id').inTable('points');
     table.integer('item_id').notNullable().references('id').inTable('items');
