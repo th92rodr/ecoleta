41f07fbb26d68f8b14fd0dc7135660410c07e61b
Thiago92Rodrigues <thiago.clrodrigues@gmail.com>
2020-06-05 17:19:29 -0300

    add prettier config file

---
+++ web/.prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+  trailingComma: 'all',
+  endOfLine: 'lf',
+  jsxBracketSameLine: false,
+  jsxSingleQuote: true,
+};
