1a3e39b8800b184056f9109e12954f1553faac35
th92rodr <45070690+th92rodr@users.noreply.github.com>
2020-06-06 15:40:45 -0300

    Server: add celebrate to validate the create-point route

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import multer from 'multer';
+import { celebrate, Joi } from 'celebrate';
 
 import multerConfig from '../config/multer';
 
@@ -17,6 +18,27 @@ router.get('/items', itemsController.index);
 router.get('/points', pointsController.index);
 router.get('/points/:id', pointsController.show);
 
-router.post('/points', upload.single('image'), pointsController.create);
+router.post(
+  '/points',
+  upload.single('image'),
+  celebrate(
+    {
+      body: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.string().required(),
+        latitude: Joi.number().required(),
+        longitude: Joi.number().required(),
+        city: Joi.string().required(),
+        uf: Joi.string().required().max(2),
+        items: Joi.string().required(),
+      }),
+    },
+    {
+      abortEarly: false,
+    },
+  ),
+  pointsController.create,
+);
 
 export default router;
