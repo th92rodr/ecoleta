27981a1f15fe1616d78690408c437d620495d927
th92rodr <45070690+th92rodr@users.noreply.github.com>
2020-06-06 01:26:20 -0300

    Server: log port when server starts

--- server/src/server.ts
+++ server/src/server.ts
@@ -12,4 +12,5 @@ app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
-app.listen(3333);
+const PORT = 3333;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
