c7414b2605cf93072332037a3fce3201b670748c
Thiago92Rodrigues <thiago.clrodrigues@gmail.com>
2020-06-06 15:13:03 -0300

    add multer in create point route

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,17 +1,22 @@
 import express from 'express';
+import multer from 'multer';
+
+import multerConfig from '../config/multer';
 
 import PointsController from '../controllers/points';
 import ItemsController from '../controllers/items';
 
 const router = express.Router();
+const upload = multer(multerConfig);
 
 const pointsController = new PointsController();
 const itemsController = new ItemsController();
 
 router.get('/items', itemsController.index);
 
-router.post('/points', pointsController.create);
 router.get('/points', pointsController.index);
 router.get('/points/:id', pointsController.show);
+
+router.post('/points', upload.single('image'), pointsController.create);
 
 export default router;
