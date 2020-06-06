6cc43a4d943ec5fea372aa88b28101cb4de96ed3
Thiago92Rodrigues <thiago.clrodrigues@gmail.com>
2020-06-06 15:40:45 -0300

    add celebrate and validation for the creadte point route

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import path from 'path';
+import { errors } from 'celebrate';
 
 import routes from './routes/index';
 
@@ -11,6 +12,8 @@ app.use(express.json());
 
 app.use(routes);
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+
+app.use(errors());
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
